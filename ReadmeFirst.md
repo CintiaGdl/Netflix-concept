3129c233ae81e38d3cd855c97f719253ef939814
CintiaGdl <cintiamgori@gmail.com>
2022-10-03 19:56:19 +0200

    improve glider

--- ReadmeFirst.md
+++ ReadmeFirst.md
@@ -1,2 +1,5 @@
 npm i
 I used axios
+glider
+no compatible react 18 para solucionarlo 
+npm i react-glider --legacy-peer-deps
--- src/components/TrendingAll.jsx
+++ src/components/TrendingAll.jsx
@@ -1,37 +1,48 @@
 import React, { useEffect, useState } from 'react'
 import { API } from '../shared/service/api';
+import Glider from "react-glider";
+import "glider-js/glider.min.css";
+
 
 export default function TrendingAll() {
     const [movie, setMovie] = useState([]);
 
     useEffect(() => {
-      
+
         API
-        .get()
-        .then((response) => {
-            setMovie(response.data.results)
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-        
-     
-    }, []);
-  
-    return (
-      <div>
-        <h1>Trending topic</h1>
-        <div className="Gallery">
-            {movie && movie
-            .map(movie => {
-                return (
-                <div key={movie.id} className='itemGallery'>
-                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                </div>
-                )
+            .get()
+            .then((response) => {
+                setMovie(response.data.results)
             })
-            }
+            .catch((error) => {
+                console.error(error);
+            });
+
+
+    }, []);
+
+    return (
+        <div>
+            <h1>Trending topic</h1>
+            <div className="Gallery">
+                <Glider
+                    hasArrows
+                    slidesToShow={4}
+                    slidesToScroll={4}
+                    scrollLock
+                    hasDots
+                >
+                    {movie && movie
+                        .map(movie => {
+                            return (
+                                <div key={movie.id} className='itemGallery'>
+                                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                                </div>
+                            )
+                        })
+                    }
+                </Glider>
+            </div>
         </div>
-      </div>
     );
 }
