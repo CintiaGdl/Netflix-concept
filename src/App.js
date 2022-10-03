45eb6af1039459cdc326634e4f9b710d3248f1fb
CintiaGdl <cintiamgori@gmail.com>
2022-10-03 10:01:13 +0200

    trendingAll component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import { useState, useEffect } from 'react';
 import './App.css';
-import { API } from './shared/service/api';
+import TrendingAll from './components/TrendingAll';
 
 function App() {
 
-  const [movie, setMovie] = useState([]);
-
-  useEffect(() => {
-    
-      API
-      .get()
-      .then((response) => {
-          setMovie(response.data.results)
-      })
-      .catch((error) => {
-          console.error(error);
-      });
-      
-   
-  }, []);
-
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      {movie && movie
-        .map(movie => {
-          return (
-            <div key={movie.id}>
-              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            </div>
-          )
-        })
-
-      }
+      <TrendingAll />
     </div>
   );
 }
---
+++ src/components/TrendingAll.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { API } from '../shared/service/api';
+
+export default function TrendingAll() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+      
+        API
+        .get()
+        .then((response) => {
+            setMovie(response.data.results)
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+        
+     
+    }, []);
+  
+    return (
+      <div className="App">
+        <h1>Trending topic</h1>
+        {movie && movie
+          .map(movie => {
+            return (
+              <div key={movie.id}>
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+              </div>
+            )
+          })
+  
+        }
+      </div>
+    );
+}
